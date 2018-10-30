768ff960ff5e97bc6ab504983dfdaa520ab8b859
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2018-10-30 15:18:11 +0000

    moreversion info

--- lib/cfg.d/00_version.pl
+++ lib/cfg.d/00_version.pl
@@ -1,5 +1,9 @@
-$c->{version_long} = "EPrints 3.3.16";
-$c->{version_alias} = "Gelato Blizzard";
-$c->{vendor_long} = "EPrints Services";
-$c->{vendor_short} = "eps";
-$c->{version_description} = $c->{version_long} . " (" . $c->{version_alias} . ") (" . $c->{vendor_long} . ")";
+$c->{base_version_long} = "EPrints 3.3.16";
+$c->{base_version_alias} = "Gelato Blizzard";
+$c->{base_vendor_long} = "EPrints Services";
+$c->{base_vendor_short} = "eps";
+$c->{version_long} = "ulcc-core 1.2";
+$c->{version_alias} = "Year of the Whopper";
+$c->{vendor_long} = "CoSector, University of London";
+$c->{vendor_short} = "cs-uoL";
+$c->{version_description} = $c->{base_version_long} ." (". $c->{base_version_alias} .") ". $c->{version_long} . " (" . $c->{version_alias} . ") (" . $c->{vendor_long} . ")";
