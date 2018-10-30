3ca8486a4a0056d825afbc79c5ff0e06bfc3b57d
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2018-10-30 15:34:10 +0000

    version stuff

--- lib/cfg.d/00_version.pl
+++ lib/cfg.d/00_version.pl
@@ -6,4 +6,4 @@ $c->{version_long} = "ulcc-core 1.2";
 $c->{version_alias} = "Year of the Whopper";
 $c->{vendor_long} = "CoSector, University of London";
 $c->{vendor_short} = "cs-uoL";
-$c->{version_description} = $c->{base_version_long} ." (". $c->{base_version_alias} .") ". $c->{version_long} . " (" . $c->{version_alias} . ") (" . $c->{vendor_long} . ")";
+$c->{version_description} = $c->{base_version_long} ." (". $c->{base_version_alias} .") => ". $c->{version_long} . " (" . $c->{version_alias} . ") (" . $c->{vendor_long} . ")";
