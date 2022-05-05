81a2e490385b6e0914cadaf611d18633a1c753a3
Will Fyson <will.fyson@cosector.com>
2022-05-05 10:55:09 +0000

    Add document id_number field as a default custom document field

--- lib/defaultcfg/cfg.d/document_fields.pl
+++ lib/defaultcfg/cfg.d/document_fields.pl
@@ -1,14 +1,9 @@
-# By default there are no custom fields in document objects, but this file
-# provides you with an example should you wish to (c.f. eprint_fields.pl)
-
-#$c->{fields}->{document} = [
-#	{
-#		name => "application",
-#		type => "set",
-#		options => [
-#			'msword95',
-#			'msword2000',
-#			'msword2007',
-#		],
-#	},
-#];
+# id_number field used for storing document DOIs (or other PIDs).
+# Primarily used by the DataCite DOI plugin
+$c->{fields}->{document} = [
+    {
+        name => "id_number",
+        type => "text",
+        render_value => 'EPrints::Extras::render_possible_doi',
+    },
+];
